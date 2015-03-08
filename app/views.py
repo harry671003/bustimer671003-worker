769edbae8efbfa04c32ffb4105f24a98252e5a80
Harry John <harry671003@gmail.com>
2015-03-08 18:46:58 +0530

    new folder structure

--- app/views.py
+++ app/views.py
@@ -1,7 +1,6 @@
 from app import application
 print "views"
 # Application routes defined here
-@application.route('/f/', endpoint='v1')
-@application.route('/index', methods=["GET", "POST"], endpoint='v2')
+@application.route('index')
 def index():
     return "Hello World!"
