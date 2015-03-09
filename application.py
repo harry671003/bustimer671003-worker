06a173c09f73e0d61c3f78297f9ef639a761c34e
Harry John <harry671003@gmail.com>
2015-03-09 10:06:25 +0530

    test

--- application.py
+++ application.py
@@ -1,9 +1,11 @@
+#!flask/bin/python
 from flask import Flask
 import os
 
 application = Flask(__name__)
 application.debug = True
 
+server_port = None
 # Default to environment variables for server port - easier for elastic beanstalk configuration
 if 'SERVER_PORT' in os.environ:
     server_port = int(os.environ['SERVER_PORT'])
