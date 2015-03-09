97e75a0a9c3a429c472a3bfb688f8d6e5944e7ac
Harry John <harry671003@gmail.com>
2015-03-09 10:04:06 +0530

    test

--- application.py
+++ application.py
@@ -6,9 +6,9 @@ application.debug = True
 
 # Default to environment variables for server port - easier for elastic beanstalk configuration
 if 'SERVER_PORT' in os.environ:
-    serverPort = int(os.environ['SERVER_PORT'])
+    server_port = int(os.environ['SERVER_PORT'])
 
-if serverPort is None:
+if server_port is None:
     serverPort = 5000
 
 @application.route('/')
@@ -16,4 +16,4 @@ def index():
 	return "Hello World"
 
 if __name__ == "__main__":
-        application.run(debug = True, port=serverPort, host='0.0.0.0')
+        application.run(debug = True, port=server_port, host='0.0.0.0')
