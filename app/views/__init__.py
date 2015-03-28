5c094618ecfa51f1e9f9983f941babe671e94180
Harry John <harry671003@gmail.com>
2015-03-28 08:35:21 +0530

    Testing sqs with db

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -2,14 +2,19 @@ from app import application, cm
 import logging
 from flask import request, Response
 import json
+from boto.dynamodb2.table import Table
 
 # Home page
 @application.route('/', methods=["GET", "POST"])
 def index():
 	response = None
-	test_table = Table('test', connection=cm.db)
-	test_table.put_item(data={
-		"id": json.dumps(request)
-	})
-	response = Response("ERROR", status=200)
+	if request.json is None:
+		# Expect application/json request
+		response = Response("NO JSON", status=415)
+	else:
+		test_table = Table('test', connection=cm.db)
+		test_table.put_item(data={
+			"id": json.dumps(request.json)
+		})
+		response = Response("ERROR", status=200)
 	return response
