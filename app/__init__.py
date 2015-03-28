1774e21252e3e1dc6d201dd000cc1e0c5fe8cfd2
Harry John <harry671003@gmail.com>
2015-03-28 08:22:59 +0530

    Testing sqs with db

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,37 @@
 from flask import Flask
+from ConfigParser import ConfigParser
+from lib.dynamodb.connection_manager import ConnectionManager
+import os
 
 
 # Create the application
 application = Flask(__name__)
 application.debug = True
 application.secret_key = "dfadfasfafa"
+
+
+# Read environment variable for whether to read config from EC2 instance metadata
+use_instance_metadata = ""
+if 'USE_EC2_INSTANCE_METADATA' in os.environ:
+    use_instance_metadata = os.environ['USE_EC2_INSTANCE_METADATA']
+
+server_port = None
+mode = None
+config = None
+# Default to environment variables for server port - easier for elastic beanstalk configuration
+if 'SERVER_PORT' in os.environ:
+	server_port = int(os.environ['SERVER_PORT'])
+	mode = 'service'
+	# Configure the application from the config file
+	config = ConfigParser()
+	config.read("config")
+else:
+    server_port = 5000
+    mode = 'local'
+
+# Connect to database with the given configurations
+cm = ConnectionManager(mode=mode, config=config, endpoint=None, port=None, use_instance_metadata=use_instance_metadata)
+
 
 # Import the views
 from app import views
--- app/views/__init__.py
+++ app/views/__init__.py
@@ -1,6 +1,7 @@
-from app import application
+from app import application, cm
 import logging
 from flask import request, Response
+import json
 
 # Home page
 @application.route('/', methods=["GET", "POST"])
@@ -10,6 +11,9 @@ def index():
 		# Expect application/json request
 		response = Response("NO JSON", status=415)
 	else:
-		logging.log('Error processing message: %s' % request.json)
+		test_table = Table('test', connection=cm.db)
+		test_table.put_item(data={
+			"id": json.dumps(request.json)
+		})
 		response = Response("ERROR", status=200)
 	return response
