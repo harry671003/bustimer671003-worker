e357397fdbb68839817da260a43eb4ad9e4e4394
Harry John <harry671003@gmail.com>
2015-03-28 09:33:33 +0530

    Testing sqs with db

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -3,15 +3,19 @@ import logging
 from flask import request, Response, jsonify
 import json
 from boto.dynamodb2.table import Table
+import base64
+from hashlib import sha1
+from datetime import datetime
 
 # Home page
 @application.route('/', methods=["PUT", "POST", "GET"])
 def index():
 	response = None
-	data = request.data
+	decoded = base64.b64decode(request.get_data())
 	test_table = Table('test', connection=cm.db)
 	test_table.put_item(data={
-		"id": data
+		"id": sha1(str(datetime.now())),
+		"data": decoded
 	})
 	response = Response("OK!", status=200)
 	return response
